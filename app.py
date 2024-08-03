1f226c1a3c0370b3b87fbcdbaec66e57cdf7fec4
jaina1008 <mailme.anshuljain@gmail.com>
2024-08-03 13:30:33 +1200

    added requirements.txt

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
-from src import key
+import os
+import streamlit as st
+from langchain_groq import ChatGroq
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
-key.groc
+from dotenv import load_dotenv 
+
+load_dotenv()
+
+# Loading GROQ and Google API key
+groq_api_key= os.getenv("GROQ_API_KEY")
