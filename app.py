df88a87f42f5061c363bdd17ca3596a5c1f11263
jaina1008 <mailme.anshuljain@gmail.com>
2024-08-04 10:20:46 +1200

    model loaded

--- app.py
+++ app.py
@@ -9,9 +9,29 @@ from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
-from dotenv import load_dotenv 
+from dotenv import load_dotenv
 
 load_dotenv()
 
 # Loading GROQ and Google API key
 groq_api_key= os.getenv("GROQ_API_KEY")
+
+st.title("Gemma Model Document Q&A")
+
+# Loading Model
+llm= ChatGroq(groq_api_key= groq_api_key,
+              model= "llama3-8b-8192")
+
+prompt= ChatPromptTemplate.from_template(
+    """
+    Answer the questions based on the provided context only.
+    Please provide the most accurate response based on the question
+    <context>
+    {context}
+    <context>
+    Questions:{input}
+"""
+)
+
+def vector_embedding():
+    
